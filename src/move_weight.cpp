ca275c4ec53f1ef9ea0c8435ac8a66d224a2a56b
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2025-12-04 21:10:18 +0700

    got them moving!

--- src/move_weight.cpp
+++ src/move_weight.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <iostream>
 
 using namespace std;
@@ -10,46 +11,21 @@ class weightObject
         sf::RenderWindow& window;
         sf::CircleShape weightShape;
         sf::Color objectColor;
+        float objectRadius;
         bool moving;
         bool selected;
     
-    public:
-        weightObject(sf::RenderWindow& windowRef, float x, float y, float radius, sf::Color color): window(windowRef)
-        {
-            sf::Vector2f position = sf::Vector2f(x, y);
-            objectColor = color;
-            weightShape.setPosition(position);
-            weightShape.setRadius(radius);
-            weightShape.setFillColor(color);
-            moving = false;
-            selected = false;
-        }
-
-        void update()
-        {
-        }
-
-        void draw()
-        {
-            window.draw(weightShape);
-        }
-
-        void move()
-        {
-        }
-
-        void select(sf::Event& event)
+        void selectObject(sf::Event& event)
         {
             if (const auto* mouseButtonReleased = event.getIf<sf::Event::MouseButtonReleased>())
             {
                 if (mouseButtonReleased->button == sf::Mouse::Button::Right)
                 {
                     sf::Vector2f mousePos = window.mapPixelToCoords(mouseButtonReleased->position);
-                    float radius = weightShape.getRadius();
-                    sf::Vector2f weightCenter = weightShape.getPosition() + sf::Vector2f(radius, radius);
+                    sf::Vector2f weightCenter = weightShape.getPosition() + sf::Vector2f(objectRadius, objectRadius);
                     sf::Vector2f distVector = mousePos - weightCenter;
                     float distanceSquare = distVector.x * distVector.x + distVector.y * distVector.y;
-                    bool withinWeight = distanceSquare <= radius * radius;
+                    bool withinWeight = distanceSquare <= objectRadius * objectRadius;
 
                     if (!selected && withinWeight)
                     {
@@ -63,35 +39,93 @@ class weightObject
                         weightShape.setFillColor(objectColor);
                         selected = false;
                     }
-                
                 }
             }
+        }
+
+        void moveObject(sf::Event& event)
+        {
+
+            if (const auto* mouseButtonPressed = event.getIf<sf::Event::MouseButtonPressed>())
+            {
+                if (mouseButtonPressed->button == sf::Mouse::Button::Left)
+                {
+                    sf::Vector2f mousePos = window.mapPixelToCoords(mouseButtonPressed->position);
+                    sf::Vector2f weightCenter = weightShape.getPosition() + sf::Vector2f(objectRadius, objectRadius);
+                    sf::Vector2f distVector = mousePos - weightCenter;
+                    float distanceSquare = distVector.x * distVector.x + distVector.y * distVector.y;
+                    if (distanceSquare <= objectRadius * objectRadius)
+                    {
+                        moving = true;
+                    }
+                }
+            }
+
+            if (moving && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+            {
+                sf::Vector2 mousePos = sf::Vector2f(sf::Mouse::getPosition(window));
+                sf::Vector2f position = mousePos - sf::Vector2f(objectRadius, objectRadius);
+                weightShape.setPosition(position);
+            }
+            else
+            {
+                moving = false;
+            }
+        }
+
+    public:
+        weightObject(sf::RenderWindow& windowRef, float x, float y, float radius, sf::Color color): window(windowRef)
+        {
+            sf::Vector2f position = sf::Vector2f(x, y);
+            objectColor = color;
+            objectRadius = radius;
+            weightShape.setPosition(position);
+            weightShape.setRadius(radius);
+            weightShape.setFillColor(color);
+            moving = false;
+            selected = false;
+        }
+
+        void update(sf::Event& event)
+        {
+            selectObject(event);
+            moveObject(event);
+            drawObject();
+        }
+
+        void drawObject()
+        {
+            window.draw(weightShape);
         }
 };
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({1000, 500}), "Moving a weight!");
+    sf::RenderWindow window(sf::VideoMode({1000, 500}), "Hello weights!!");
     vector<weightObject> weights {
         weightObject(window, 40, 50, 20.0, sf::Color::Yellow),
         weightObject(window, 140, 350, 40.0, sf::Color::Green)
     };
+    sf::Color background = sf::Color(100,100,100);
 
-    printf("hello\n");
+    printf("Hello weights!!\n");
     while (window.isOpen())
     {
+        window.clear(background);
+        for (unsigned int i=0; i < weights.size(); i++) {
+            weights[i].drawObject();
+        }
+        window.display();
+
         while (optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-                window.close();
-        
-                
-            window.clear(sf::Color(100,100,100));
-            weights[0].select(*event);
-            weights[1].select(*event);
-            weights[0].update();
-            weights[0].draw();
-            weights[1].draw();
+            window.close();
+            
+            window.clear(background);
+            for (unsigned int i=0; i < weights.size(); i++) {
+                weights[i].update(*event);
+            }
             window.display();
         }
     }
